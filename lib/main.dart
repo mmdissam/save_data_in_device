e80e247de18b5e39feeeab2b04c8ce988168fa73
NiMo- <mo5-98@hotmail.com>
2020-04-07 01:03:25 +0300

    methods of write and read data on device

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:savedataindevice/GUI/home.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,7 +12,28 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      home:Scaffold(),
+      home:Home(),
     );
+  }
+}
+Future<String> get localPath async{
+  final path = await getApplicationDocumentsDirectory();
+  return path.path;
+}
+Future<File> get localFile async{
+  final file = await localPath;
+  return new File('$file/data.txt');
+}
+Future<File> writeData(String value) async{
+  final file = await localFile;
+  return file.writeAsString('$value');
+}
+Future<String> readData() async{
+  try{
+      final file = await localFile;
+      String data = await file.readAsString();
+      return data;
+  }catch(e){
+    return 'error: file not found';
   }
 }
